a0b9ec168a2df7ce72240740de06700fff5c1d43
Brandon <brandon1324@hotmail.com>
2017-11-26 00:59:12 +0100

    Añadida una persistencia simple

--- src/Cerebro.java
+++ src/Cerebro.java
@@ -1,5 +1,54 @@
+import java.io.*;
 import java.util.ArrayList;
 
-public class Cerebro {
-    ArrayList<Neurona> neuronas;
+public class Cerebro{
+    public static ArrayList<Neurona> neuronas;
+
+    public Cerebro() {
+        try{
+            load();
+        }catch (Exception e){
+            neuronas = new ArrayList<Neurona>();
+        }
+
+        try{
+            for(Neurona n : neuronas){
+                n.start();
+            }
+        }catch (NullPointerException err){
+
+        }
+
+    }
+    public void save(){
+        try {
+            FileOutputStream fos = new FileOutputStream("recuerdos.txt");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(neuronas); // write MenuArray to ObjectOutputStream
+            oos.close();
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    public void load() throws Exception{
+            FileInputStream fos = new FileInputStream("recuerdos.txt");
+            ObjectInputStream input = new ObjectInputStream(fos);
+            neuronas = (ArrayList<Neurona>) input.readObject();
+            input.close();
+    }
+    public void añadirNeurona(Neurona n){
+        neuronas.add(n);
+        n.start();
+        save();
+    }
+    public void crearNeurona(){
+        neuronas.add(new Neurona());
+        neuronas.get(neuronas.size() - 1).start();
+        save();
+    }
+    public static void main(String[] args){
+        Cerebro c = new Cerebro();
+        System.out.println(neuronas);
+    }
+
 }
--- src/Neurona.java
+++ src/Neurona.java
@@ -1,6 +1,25 @@
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Neurona {
+public class Neurona extends Thread implements Serializable {
     ArrayList<Neurona> conectadas;
-    
+
+    public void conectarCon(Neurona n) {
+        conectadas.add(n);
+    }
+    public boolean desconectar(Neurona n){
+        return conectadas.remove(n);
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            System.out.println(getId());
+            try {
+                sleep(250);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
