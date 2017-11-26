7097ed6f70177c5848ba88cd47c5612c93bb2193
Brandon <brandon1324@hotmail.com>
2017-11-26 04:25:00 +0100

    Añadida una persistencia simple

--- src/Cerebro.java
+++ src/Cerebro.java
@@ -1,54 +1,87 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Cerebro{
+public class Cerebro {
     public static ArrayList<Neurona> neuronas;
 
     public Cerebro() {
-        try{
+        try {
             load();
-        }catch (Exception e){
+        } catch (Exception e) {
             neuronas = new ArrayList<Neurona>();
         }
 
-        try{
-            for(Neurona n : neuronas){
+        try {
+            for (Neurona n : neuronas) {
+                System.out.println(n);
                 n.start();
             }
-        }catch (NullPointerException err){
+        } catch (NullPointerException err) {
 
         }
 
     }
-    public void save(){
+
+    public void save() {
         try {
             FileOutputStream fos = new FileOutputStream("recuerdos.txt");
             ObjectOutputStream oos = new ObjectOutputStream(fos);
             oos.writeObject(neuronas); // write MenuArray to ObjectOutputStream
             oos.close();
-        } catch(Exception ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
-    public void load() throws Exception{
-            FileInputStream fos = new FileInputStream("recuerdos.txt");
-            ObjectInputStream input = new ObjectInputStream(fos);
-            neuronas = (ArrayList<Neurona>) input.readObject();
-            input.close();
+
+    public void load() throws Exception {
+        FileInputStream fos = new FileInputStream("recuerdos.txt");
+        ObjectInputStream input = new ObjectInputStream(fos);
+        neuronas = (ArrayList<Neurona>) input.readObject();
+        input.close();
     }
-    public void añadirNeurona(Neurona n){
+
+    public void añadirNeurona(Neurona n) {
         neuronas.add(n);
         n.start();
         save();
     }
-    public void crearNeurona(){
+
+    public void crearNeurona() {
         neuronas.add(new Neurona());
         neuronas.get(neuronas.size() - 1).start();
         save();
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Cerebro c = new Cerebro();
-        System.out.println(neuronas);
+        for(int i = 0; i<200;i++){
+            c.crearNeurona();
+        }
+        System.out.println("Empezamos el programa");
+        System.out.println("Por favor introduzca una cadena por teclado:");
+        String entradaTeclado = "";
+        Scanner entradaEscaner = new Scanner(System.in);
+        entradaTeclado = entradaEscaner.nextLine();
+        System.out.println("Entrada recibida por teclado es: \"" + entradaTeclado + "\"");
+        c.neuronas.get(1).conectarCon(c.neuronas.get(2));
+        System.out.println(c);
     }
 
+    public static ArrayList<Neurona> getNeuronas() {
+        return neuronas;
+    }
+
+    public static void setNeuronas(ArrayList<Neurona> neuronas) {
+        Cerebro.neuronas = neuronas;
+    }
+
+    @Override
+    public String toString() {
+        String res = "";
+        for(Neurona n : neuronas){
+            res = res + n + " -- " + n.conectadas + "\n";
+        }
+        return res;
+    }
 }
--- src/Neurona.java
+++ src/Neurona.java
@@ -3,9 +3,21 @@ import java.util.ArrayList;
 
 public class Neurona extends Thread implements Serializable {
     ArrayList<Neurona> conectadas;
+    public Neurona(){
+        conectadas = new ArrayList<Neurona>();
+    }
+
+    public Neurona(ArrayList<Neurona> n){
+        conectadas = n;
+    }
+
+    public ArrayList<Neurona> getConectadas() {
+        return conectadas;
+    }
 
     public void conectarCon(Neurona n) {
         conectadas.add(n);
+        n.conectadas.add(this);
     }
     public boolean desconectar(Neurona n){
         return conectadas.remove(n);
@@ -14,12 +26,18 @@ public class Neurona extends Thread implements Serializable {
     @Override
     public void run() {
         while(true){
-            System.out.println(getId());
             try {
                 sleep(250);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    @Override
+    public String toString() {
+        String res = "";
+        res = "Neurona: " + getId();
+        return res;
     }
 }
